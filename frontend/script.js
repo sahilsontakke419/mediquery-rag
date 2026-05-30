57bce10bc0b0b2619a70ae507cf53de4063acdab
sahilsontakke419 <83176570+sahilsontakke419@users.noreply.github.com>
2026-05-31 03:00:57 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,4 @@
-const API = "http://localhost:8000";
-
+const API = "https://mediquery-rag.onrender.com";
 const uploadArea = document.getElementById('uploadArea');
 const pdfFile = document.getElementById('pdfFile');
 
